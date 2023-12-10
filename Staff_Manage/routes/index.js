8717fa7fea216a35bd125c676392b2c372b278d1
Fiza Razzaq <frazzaq335@gmail.com>
2023-12-10 23:45:36 +0500

    Pug Files Added

--- Staff_Manage/routes/index.js
+++ Staff_Manage/routes/index.js
@@ -3,7 +3,20 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  //res.send("Hello from routes");
   res.render('index', { title: 'Express' });
+});
+
+/* GET add page. */
+router.get('/add', function(req, res, next) {
+  //res.send("Hello from routes");
+  res.render('create', { title: 'Express' });
+});
+
+/* GET show page. */
+router.get('/show', function(req, res, next) {
+  //res.send("Hello from routes");
+  res.render('show', { title: 'Express' });
 });
 
 module.exports = router;
